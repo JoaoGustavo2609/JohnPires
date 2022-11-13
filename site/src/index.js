dd91ae73221b370b440e9be405c5630055c6cda0
JoaoGustavo2609 <johngithub2609@gmail.com>
2022-11-13 01:13:06 -0300

    Pastas e import

--- site/src/index.js
+++ site/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-
+import LandingPage from './pages/landingpage'
+import CadastroUsuario from './pages/cadastrousuario'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +12,8 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route/>
+        <Route path='/landingpage' element={<LandingPage/>}                   />
+        <Route path='/cadastrousuario' element={<CadastroUsuario/>}           />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ site/src/pages/CadastroUsuario/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+export default function CadastroUsu() {
+    return (
+        <main>
+            
+        </main>
+    );
+}
